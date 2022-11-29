705baed62b8b0c088b81d3bdfccb3e355b97f521
Chantal Wuerschinger <chantalwue@gmail.com>
2022-11-29 12:28:41 +0100

    Started packaging our code created params.py, preprocessor.py not running without issues yet

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ with open('requirements.txt') as f:
 requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='earthquake_damage',
-      version="1.0",
+    #   version="1.0",
       description="Project Description",
       packages=find_packages(),
       install_requires=requirements,
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
-      include_package_data=True,
-      scripts=['scripts/earthquake_damage-run'],
-      zip_safe=False)
+    #   include_package_data=True,
+    #   scripts=['scripts/earthquake_damage-run'],
+    #   zip_safe=False
+      )
