656e72489cb8b37f82ac7da4dcd11ef4f4f22680
Chuting Cao <janecao1213@outlook.com>
2022-11-30 17:39:50 +0100

    model_stage_1

--- earthquake_damage/data/main.py
+++ earthquake_damage/data/main.py
@@ -7,14 +7,14 @@ from sklearn.model_selection import train_test_split
 my_name = os.environ.get("MY_NAME")
 
 
-b_structure = pd.read_csv('/Users/chantalwuerschinger/code/chantalwuer/earthquake_damage/raw_data/building/csv_building_structure.csv')
-b_owner_use = pd.read_csv('/Users/chantalwuerschinger/code/chantalwuer/earthquake_damage/raw_data/building/csv_building_ownership_and_use.csv')
+b_structure = pd.read_csv(f'/Users/{my_name}/code/chantalwuer/earthquake_damage/raw_data/building/csv_building_structure.csv')
+b_owner_use = pd.read_csv(f'/Users/{my_name}/code/chantalwuer/earthquake_damage/raw_data/building/csv_building_ownership_and_use.csv')
 
 
 merged_df = drop_and_merge(b_structure, b_owner_use)
 
 
-def train_test_val(df, test_size=0.3):
+def train_test_val(df, test_size=0.3, random_state=42):
     """
     This function defines X and y,
     then splits the data into a training, test and validation set.
@@ -22,9 +22,9 @@ def train_test_val(df, test_size=0.3):
     X = df.drop(['damage_grade', 'building_id'], axis=1)
     y = df['damage_grade']
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
     # Split the data into train and test
-    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5)
+    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5,random_state)
 
     return X_train, X_test, X_val, y_train, y_test, y_val
