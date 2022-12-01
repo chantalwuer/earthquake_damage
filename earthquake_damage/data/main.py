e5203faba512c61fe9532942baa41b074902d0cf
Chantal Wuerschinger <chantalwue@gmail.com>
2022-12-01 09:06:10 +0100

    Added random_state to train_test_split

--- earthquake_damage/data/main.py
+++ earthquake_damage/data/main.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 my_name = os.environ.get("MY_NAME")
 
 
-def train_test_val(test_size=0.3):
+def train_test_val(test_size=0.3, random_state=42):
     """
     This function defines X and y,
     then splits the data into a training, test and validation set.
@@ -18,9 +18,9 @@ def train_test_val(test_size=0.3):
     # y = df['damage_grade']
 
     # Split the data into train and test
-    X_train, X_test, y_train, y_test = train_test_split(X_processed, y_processed, test_size=test_size)
+    X_train, X_test, y_train, y_test = train_test_split(X_processed, y_processed, test_size=test_size, random_state=random_state)
 
     # Split the data into test and val
-    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5)
+    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=random_state)
 
     return X_train, X_test, X_val, y_train, y_test, y_val
