b644f69d95e732b5b1db72596f4fe527564e7288
Chantal Wuerschinger <chantalwue@gmail.com>
2022-12-08 23:05:24 +0100

    Upgrading manifest files for package deployment

--- setup.py
+++ setup.py
@@ -7,13 +7,16 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-file = os.path.join(os.path.dirname(__file__), 'eathquake_damage/processed_data/comp_data_household.csv')
 setup(name='earthquake_damage',
       description="Project Description",
       packages=find_packages(),
       install_requires=requirements,
       test_suite='tests',
       include_package_data=True,
-      data_files=[ file ],
+      data_files=[
+          os.path.join(os.path.dirname(__file__), 'processed_data/comp_data_household.csv'),
+          os.path.join(os.path.dirname(__file__), 'earthquake_damage/data/preprocessor.pkl'),
+          os.path.join(os.path.dirname(__file__), 'Streamlit/fit_best_model.pkl')
+      ],
       zip_safe=False
       )
